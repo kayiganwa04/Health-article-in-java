84486da5ef15cdedc3f01d9c2365d33a64bb8490
kayiganwa04 <79788219+kayiganwa04@users.noreply.github.com>
2021-12-19 11:53:56 +0200

    Add files via upload

---
+++ me (4).sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 05, 2021 at 05:13 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `me`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(125) NOT NULL,
+  `username` varchar(233) NOT NULL,
+  `user_email` varchar(233) NOT NULL,
+  `user_password` varchar(233) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `user_email`, `user_password`) VALUES
+(1, 'admin', 'admin@gmail.com', 'pass');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(125) NOT NULL,
+  `title` varchar(125) NOT NULL,
+  `author` varchar(125) NOT NULL,
+  `Description` varchar(5000) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `author`, `Description`, `created_at`) VALUES
+(19, 'retyuiop', 'ewrtyuiop', 'ret6yuiop', '2021-12-05 14:38:39'),
+(20, 'wetruiyo', 'tghyjkl', 'wstgrhl', '2021-12-05 14:41:31'),
+(21, 'dsghhjj', 'hgjhjhm', 'hgjhgjhjjggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg', '2021-12-05 14:42:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_user`
+--
+
+CREATE TABLE `post_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `post_user`
+--
+
+INSERT INTO `post_user` (`id`, `username`, `user_email`, `user_password`) VALUES
+(17, 'me', 'me@gmail.com', 'me'),
+(18, 'you', 'you@gmail.com', 'you');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `post_user`
+--
+ALTER TABLE `post_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(125) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(125) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `post_user`
+--
+ALTER TABLE `post_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
